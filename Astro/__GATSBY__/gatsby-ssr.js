66c055ed8cf6877c9cbd2701c818583938beae17
rr-h <137926547+rr-h@users.noreply.github.com>
2024-03-17 20:56:46 +0000

    r

--- Astro/__GATSBY__/gatsby-ssr.js
+++
@@ -1,3 +0,0 @@
-export { default as wrapPageElement } from './wrapPageElement';
-export { default as wrapRootElement } from './wrapRootElement';
-export { default as onRenderBody } from './onRenderBody';
